089b655aa21612e7d6858826e5319283ccc47fd5
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-04-03 16:18:04 +0300

    Update landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,12 @@ import { Trans, withI18n } from '@lingui/react'
 
 import logo from '../assets/images/logo.svg'
 import badge from '../assets/images/badge.svg'
+import family from '../assets/images/family.jpg'
+import grapes from '../assets/images/grapes.jpg'
+import wines from '../assets/images/wines.jpg'
+import party1 from '../assets/images/party1.jpg'
+import restaurant from '../assets/images/restaurant.jpg'
 
-// <div>
-//   <h1>
-//     <Trans>Hi people</Trans>
-//   </h1>
-//   <p><Trans>Welcome to your new Gatsby site.</Trans></p>
-//   <Trans render="p">Now go build something great.</Trans>
-//   <Link to={i18n.t`/page-2`}><Trans>Go to page 2</Trans></Link>
-// </div>
 const IndexPage = ({ i18n }) => (
   <div className='startseite content-container'>
     <header>
@@ -26,14 +23,33 @@ const IndexPage = ({ i18n }) => (
     </header>
 
     <section>
-      <div className='image' style={{ backgroundImage: `url('./assets/images/family.jpg')` }} />
+      <div className='image' style={{ backgroundImage: `url(${family})` }} />
       <div />
     </section>
 
-    {/* <section className='reverse'>
-      <div className='image' style='background-image: url(&#39;/assets/images/grapes.jpg&#39;);' />
+    <section className='reverse'>
+      <div className='image' style={{ backgroundImage: `url(${grapes})` }} />
       <div />
-    </section> */}
+    </section>
+
+    <section>
+      <div className='image' style={{ backgroundImage: `url(${wines})` }} />
+      <div className='cta'>
+        <a href='weine'>Mehr über unsere Weine</a>
+      </div>
+    </section>
+
+    <section className='reverse'>
+      <div className='image' style={{ backgroundImage: `url(${party1})` }} />
+      <div />
+    </section>
+
+    <section>
+      <div className='image' style={{ backgroundImage: `url(${restaurant})` }} />
+      <div className='cta'>
+        <a href='weine'>Mehr über unsere Weine</a>
+      </div>
+    </section>
   </div>
 )
 
