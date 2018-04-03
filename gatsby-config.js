320c9bc90451c0f917f5a0b4d851712256b468f8
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-04-03 16:51:23 +0300

    Update package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter'
   },
-  plugins: [ 'gatsby-plugin-react-helmet', 'gatsby-plugin-react-next', 'gatsby-plugin-sass' ]
+  plugins: [ 'gatsby-plugin-react-helmet', 'gatsby-plugin-react-next', 'gatsby-plugin-postcss-sass' ]
 }
