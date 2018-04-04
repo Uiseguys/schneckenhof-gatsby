026718c1fd45544f24504c7f760872c526c5af56
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-04-04 16:04:43 +0300

    Add more styling Add subpage header Add weine page

---
+++ src/components/SubpageHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { logo } from '../assets'
+export default () => (
+  <header class='subpage'>
+    <div class='branding'>
+      <a href='/'>
+        <img src={logo} />
+      </a>
+    </div>
+  </header>
+)
