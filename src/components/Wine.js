8e3519ffa6b0a0e81dad9e24911777818502ed72
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-05-24 12:51:50 +0300

    Show price on wine even if packaging is null Show '00' decimals on wine price

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -10,7 +10,7 @@ const priceDecimals = (price) => Math.round((price - priceInt(price)) * 100)
 
 const PriceInt = ({ price }) => <span className='price__int'>{priceInt(price)}</span>
 
-const PriceDec = ({ price }) => <span className='price__decimals'>{priceDecimals(price) ? priceDecimals(price) : ''}</span>
+const PriceDec = ({ price }) => <span className='price__decimals'>{priceDecimals(price) ? priceDecimals(price) : '00'}</span>
 
 export default ({ wine }) => (
   <div className='simpleCart_shelfItem c-wine'>
@@ -34,9 +34,9 @@ export default ({ wine }) => (
         )}
 
         <AddToCart item={wine} />
-        
-        {wine.packaging && (
-          <div className='wine__costs'>
+
+        <div className='wine__costs'>
+          {wine.packaging && (
             <span className='content'>
               <span className='item_content'>{wine.packaging.displayName || wine.packaging.measure + wine.packaging.unitOfMeasure}</span>
               {/* {wine.content < 1 && <span className='liter-price'>{accounting.formatMoney(wine.price / (wine.content * 100) * 100, '€', 2, '.', ',')}/l</span>} */}
@@ -44,15 +44,13 @@ export default ({ wine }) => (
                 <span className='liter-price'>{accounting.formatMoney(wine.price / wine.packaging.measure * 1000, '€', 2, '.', ',')}/l</span>
               )}
             </span>
+          )}
 
-            <span className='item_price visuallyhidden'>{wine.price}</span>
-
-            <span className='c-price'>
-              <PriceInt price={wine.price} />
-              <PriceDec price={wine.price} />
-            </span>
-          </div>
-        )}
+          <span className='c-price'>
+            <PriceInt price={wine.price} />
+            <PriceDec price={wine.price} />
+          </span>
+        </div>
       </div>
     </div>
   </div>
