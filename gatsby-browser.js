ce81d0aa7e166aeb14d4f58035fa0d814eb331cb
dennis.croitoru@gmail.com <dennis.croitoru@gmail.com>
2018-05-29 16:31:50 +0300

    Use the plugin for GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,19 +8,6 @@ import { Provider } from 'react-redux'
 
 import createStore from './src/state/createStore'
 
-exports.onClientEntry = () => {
-  var _gaq = _gaq || []
-  _gaq.push([ '_setAccount', 'UA-3744376-1' ])
-  _gaq.push([ '_trackPageview' ])
-
-  var ga = document.createElement('script')
-  ga.type = 'text/javascript'
-  ga.async = true
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(ga, s)
-}
-
 exports.replaceRouterComponent = ({ history }) => {
   const store = createStore()
 
