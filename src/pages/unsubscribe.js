b29b1551099b38fd8e03ccef6c496d07a7aa6210
Cris Shaki <cmshaki@gmail.com>
2019-11-01 18:09:14 +0300

    Added unsubscribe page

---
+++ src/pages/unsubscribe.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import SubpageHeader from "../components/SubpageHeader";
+
+export default () => (
+  <div className="content-container" id="unsubscribe-body">
+    <SubpageHeader />
+
+    <h2>YOU HAVE SUCCESSFULY UNSUBSCRIBED</h2>
+    <div className="subpage-content">
+      <div className="text">
+        <p>
+          <span>😔</span>
+          We are sorry to see you go, remember you can always resubscribe to our
+          newsletter by entering your email below on our footer
+        </p>
+      </div>
+    </div>
+  </div>
+);
